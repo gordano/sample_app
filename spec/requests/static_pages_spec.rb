39d1b6c13d20dc25cd939df0fb7ad28cd12daa4e
Gordano <headbat@gmail.com>
2015-05-30 19:27:18 +0300

    Созданы тесты и статические страницы конец 3 главы

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
+    end
+  end
+
+  describe "Help page" do
+  it "should have the content 'Help'" do
+    visit '/static_pages/help'
+    expect(page).to have_title("#{base_title} | Help")
+  end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+end
