0fe592c76b617812ce463975746e7b09ec1013f4
Gordano <headbat@gmail.com>
2015-05-31 03:36:24 +0300

    Конец главы 4

--- Gemfile
+++ Gemfile
@@ -42,12 +42,16 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'rspec-rails'
+  gem 'guard-rspec'
 end
 
 group :test do
   gem 'capybara'
   #gem 'factory_girl_rails'
   gem 'selenium-webdriver'
+  gem 'libnotify'
+
+
 
 end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
+
+describe "Home page" do
+
+  it "should have the content 'Sample App'" do
+    visit '/static_pages/home'
+    expect(page).to have_content('Sample App')
   end
+
+  it "should have the base title" do
+    visit '/static_pages/home'
+    expect(page).to have_title("#{base_title}")
+  end
+=begin
+  it "should not have a custom page title" do
+    visit '/static_pages/home'
+    expect(page).not_to have_title('| Home')
+  end
+=end
+end
 
   describe "Help page" do
   it "should have the content 'Help'" do
