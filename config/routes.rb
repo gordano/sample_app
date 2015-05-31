230dfae3c986e5bff8f2013510ba9cae8de07586
Gordano <headbat@gmail.com>
2015-05-31 05:43:00 +0300

    Улучшены тесты для StaticPages (переход к созданию регистрации)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+
+  #get 'static_pages/home'
+  root 'static_pages#home'
+  #get 'static_pages/help'
+  #match '/', to: 'static_pages#home', via: 'get'
+  match '/about', to: 'static_pages#about',  via: 'get'
+  match '/help', to: 'static_pages#help',  via: 'get'
+  match '/contact', to: 'static_pages#contact',  via: 'get'
+  #get 'static_pages/contact'
+
+  #match '/help',    to: 'static_pages_help_path#help',    via: 'get'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
